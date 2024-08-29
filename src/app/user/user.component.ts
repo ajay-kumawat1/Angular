fa2aecd39d0b69897bcc2e026449b8f09c589b06
Ajay <ajaykumawat9904@gmail.com>
2024-08-29 14:16:29 +0530

    reusing components with input properties

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomUser = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -11,15 +8,12 @@ const randomUser = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomUser]);
-  imagePath = computed(() => '/assets/users/'+ this.selectedUser().avatar);
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
 
-  // get imagePath() {
-  //   return '/assets/users/'+ this.selectedUser.avatar
-  // }
-
-  onSelectUser() {
-    const randomUser = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser = signal(DUMMY_USERS[randomUser]);
+  get imagePath() {
+    return `assets/users/${this.avatar}`;
   }
+
+  onSelectUser() {}
 }
